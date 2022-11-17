a4937e077f0e1b61f752631967b492ca1e66bcdd
Khaled Tanveer <khaledtanveer@hscs-mba-1753.12x158.nycboe.org>
2022-11-17 12:52:29 -0500

    new functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,55 @@
 import "./App.css";
 import { useState } from "react";
+import Userpoint from "./Userpoint";
 
 function App() {
-    const [points, setPoints] = useState([]);
-    const [deletedPoints, setDeletedPoints] = useState([]);
-    const handleClick = (event) => {
-        return setPoints([
-            ...points,
-            {
-                x: event.clientX,
-                y: event.clientY,
-            },
-        ]);
-    };
-    const handleUndo = () => {
-        setDeletedPoints([...deletedPoints, points.pop()]);
-        setPoints([...points]);
-    };
-    const handleRedo = () => {
-        if (deletedPoints.length > 0) {
-            setPoints([...points, deletedPoints.pop()]);
-            setDeletedPoints([...deletedPoints]);
-        }
-    };
-    return (
-        <div className="App">
-            <div>
-                <button type="button" onClick={handleUndo}>
-                    Undo
-                </button>
-                <button type="button" onClick={handleRedo}>
-                    Redo
-                </button>
-            </div>
-            <div className="area" onClick={handleClick}>
-                {points.map((point, index) => {
-                    if (point) {
-                        return (
-                            <div
-                                key={index}
-                                className="point"
-                                style={{ top: point.y, left: point.x }}
-                            />
-                        );
-                    }
-                })}
-            </div>
-        </div>
-    );
+  const [points, setPoints] = useState([]);
+  const [deletedPoints, setDeletedPoints] = useState([]);
+  const handleClick = (event) => {
+    return setPoints([
+      ...points,
+      {
+        x: event.clientX,
+        y: event.clientY,
+      },
+    ]);
+  };
+  const handleUndo = () => {
+    setDeletedPoints([...deletedPoints, points.pop()]);
+    setPoints([...points]);
+  };
+  const handleRedo = () => {
+    if (deletedPoints.length > 0) {
+      setPoints([...points, deletedPoints.pop()]);
+      setDeletedPoints([...deletedPoints]);
+    }
+  };
+  return (
+    <div className="App">
+      <Userpoint />
+      <div>
+        <button type="button" onClick={handleUndo}>
+          Undo
+        </button>
+        <button type="button" onClick={handleRedo}>
+          Redo
+        </button>
+      </div>
+      <div className="area" onClick={handleClick}>
+        {points.map((point, index) => {
+          if (point) {
+            return (
+              <div
+                key={index}
+                className="point"
+                style={{ top: point.y, left: point.x }}
+              />
+            );
+          }
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
